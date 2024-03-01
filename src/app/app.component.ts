9af480c64ae489418ea03fa6ef90be11639db29f
mudassarali.official <mudassarali.official@gmail.com>
2024-03-01 05:37:56 +0500

    Data Bind In Angular Complete Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DataBindingInAngular';
+  name = 'Angular';
+  imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+  isDisabled = false;
+  isChecked=false;
+
+  clickMe() {
+    console.log('Button Clicked');
+    alert('Button Clicked');
+  }
+
+  enableButton() {
+    this.isDisabled = false;
+  }
+
+  Checked() {
+    this.isDisabled = true;
+  }
 }
